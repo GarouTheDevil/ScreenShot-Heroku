e134ab8d3a358a881f7b9daccdb1c813760fc90c
David-raj123 <64373160+David-raj123@users.noreply.github.com>
2021-08-03 21:02:23 +0530

    Update utils.py

--- bot/utils/utils.py
+++ bot/utils/utils.py
@@ -112,8 +112,8 @@ class Utilities:
                 ''.join([f"◽" for i in range(math.floor(percentage / 10))]),
                 ''.join([f"◾" for i in range(10 - math.floor(percentage / 10))])
             )
-            current_message = f"**Downloading ⬇️ :** {round(percentage, 2)}%\n\n"
-            current_message += f"{progressbar}\n\n"
+            current_message = f"**Downloading ⬇️ :** {round(percentage, 2)}%\n"
+            current_message += f"{progressbar}\n"
             current_message += f"{HOLLOW_RED_CIRCLE} **Speed** : {Utilities.humanbytes(speed)}/s\n"
             current_message += f"{HOLLOW_RED_CIRCLE} **Done** : {Utilities.humanbytes(current)}\n"
             current_message += f"{HOLLOW_RED_CIRCLE} **Size** : {Utilities.humanbytes(total)}\n"
